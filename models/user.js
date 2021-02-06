da39d8f20707fb12dba7a4ece2a9555707d6d760
brendonstahl97 <brendonstahl97@gmail.com>
2021-02-06 13:51:57 -0500

    added schema methods

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  userId: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  userId: { type: String, unique: true, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, unique: false, required: true },
   goals: [
     {
       name: { type: String, required: true },
@@ -18,6 +18,27 @@ const userSchema = new Schema({
       ] 
     }
   ]
+});
+
+// Define schema methods
+userSchema.methods = {
+	checkPassword: function(inputPassword) {
+		return bcrypt.compareSync(inputPassword, this.password);
+	},
+	hashPassword: plainTextPassword => {
+		return bcrypt.hashSync(plainTextPassword, 10);
+	}
+}
+
+// Define hooks for pre-saving
+userSchema.pre('save', function(next) {
+	if (!this.password) {
+		console.log('No password provided!');
+		next();
+	} else {
+		this.password = this.hashPassword(this.password);
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
