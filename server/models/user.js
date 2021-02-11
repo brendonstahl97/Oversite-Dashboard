a8119a1f101c53f4e6669064ad9a4b853b86725b
brendonstahl97 <brendonstahl97@gmail.com>
2021-02-11 13:10:51 -0500

    integrated all authentication code and restructured directory

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  firstName: { type: String, unique: false },
+  lastName: { type: String, unique: false },
   userId: { type: String, unique: true, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, unique: false, required: true },
@@ -15,28 +17,28 @@ const userSchema = new Schema({
           value: Number,
           date: String
         }
-      ] 
+      ]
     }
   ]
 });
 
 // Define schema methods
 userSchema.methods = {
-	checkPassword: function(inputPassword) {
-		return bcrypt.compareSync(inputPassword, this.password);
-	},
-	hashPassword: plainTextPassword => {
-		return bcrypt.hashSync(plainTextPassword, 10);
-	}
+  checkPassword: function (inputPassword) {
+    return bcrypt.compareSync(inputPassword, this.password);
+  },
+  hashPassword: plainTextPassword => {
+    return bcrypt.hashSync(plainTextPassword, 10);
+  }
 }
 
 // Define hooks for pre-saving
-userSchema.pre('save', function(next) {
-	if (!this.password) {
-		console.log('No password provided!');
-		next();
-	} else {
-		this.password = this.hashPassword(this.password);
+userSchema.pre('save', function (next) {
+  if (!this.password) {
+    console.log('No password provided!');
+    next();
+  } else {
+    this.password = this.hashPassword(this.password);
     next();
   }
 });
