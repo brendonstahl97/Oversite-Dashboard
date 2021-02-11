cb247287e3d549722c9b5e16d3d9740b0b996abb
brendonstahl97 <brendonstahl97@gmail.com>
2021-02-11 13:21:08 -0500

    fixed get route for "/" in routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/'))
+  res.sendFile(path.join(__dirname, '../../client/build/'))
 });
 
 
