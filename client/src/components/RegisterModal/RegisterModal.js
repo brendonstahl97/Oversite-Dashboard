dac0b124ee464800ff92fa30d41d801e848f424e
emelanson <elijahmelanson@gmail.com>
2021-02-11 19:14:15 -0500

    Created splash screen and redirect to dashboard

---
+++ client/src/components/RegisterModal/RegisterModal.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import useAxios from "axios";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardText,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
+
+const RegisterModal = (props) => {
+  const history = useHistory();
+
+  const [userState, setUserState] = useState({
+    userId: "",
+    email: "",
+    password: "",
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    console.table(userState);
+    console.log('Button click ...');
+
+    useAxios.post('http//localhost:3000/api/user', { userState }).then((res) => console.log(res));
+
+    history.push('/admin/dashboard')
+  };
+
+  const updateState = (e) => {
+    setUserState({
+      ...userState,
+      [e.target.name]: e.target.value
+    });
+  };
+
+
+  return (
+    <>
+      <Row>
+        <Col md="8">
+          <Card>
+            <CardHeader>
+              <h5 className="title">Signup</h5>
+            </CardHeader>
+            <CardBody>
+              <Form>
+                <Row>
+                  <Col className="px-md-1" md="3">
+                    <FormGroup>
+                      <label>Username</label>
+                      <Input
+                        defaultValue=""
+                        placeholder="Username"
+                        name="userId"
+                        type="text"
+                        onChange={updateState}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col className="pl-md-1" md="4">
+                    <FormGroup>
+                      <label htmlFor="exampleInputEmail1">
+                        Email address
+                        </label>
+                      <Input placeholder="" name="email" type="email" onChange={updateState} />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="pr-md-1" md="6">
+                    <FormGroup>
+                      <label>Password</label>
+                      <Input
+                        placeholder=""
+                        name="password"
+                        type="password"
+                        onChange={updateState}
+                      />
+                    </FormGroup>
+                  </Col>
+                </Row>
+              </Form>
+            </CardBody>
+            <CardFooter>
+              <Button className="btn-fill" color="primary" type="submit" onClick={handleClick}>
+                Submit
+                </Button>
+            </CardFooter>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default RegisterModal;
