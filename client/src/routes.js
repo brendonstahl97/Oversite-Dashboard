f62e1b1f61eaff375a6822581bd9ec92a837ce39
BetterBusinessBrian <bman1402@gmail.com>
2021-02-13 11:21:41 -0500

    trimming extra views and dashboard features

--- client/src/routes.js
+++ client/src/routes.js
@@ -16,12 +16,6 @@
 
 */
 import Dashboard from "views/Dashboard.js";
-import Icons from "views/Icons.js";
-// import Map from "views/Map.js";
-import Notifications from "views/Notifications.js";
-import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import UserProfile from "views/UserProfile.js";
 import Goals from "views/Goals.js";
 
 import newGoal from "views/goalInput.js";
@@ -47,42 +41,6 @@ var routes = [
     name: "New Goal",
     icon: "tim-icons icon-trophy",
     component: newGoal,
-    layout: "/admin",
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "tim-icons icon-atom",
-    component: Icons,
-    layout: "/admin",
-  },
-
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: "/admin",
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: "/admin",
-  },
-  {
-    path: "/tables",
-    name: "Table List",
-    icon: "tim-icons icon-puzzle-10",
-    component: TableList,
-    layout: "/admin",
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "tim-icons icon-align-center",
-    component: Typography,
     layout: "/admin",
   },
   {
