0395d34b9d8ba518a194a49488bca5679ce213b0
emelanson <elijahmelanson@gmail.com>
2021-02-16 21:05:05 -0500

    Finished goalInput state and axios post of new goal format.

--- client/src/views/goalInput.js
+++ client/src/views/goalInput.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Row, Col, Card, CardHeader, CardTitle, Button, Form, FormGroup, Label, Input, CardBody } from 'reactstrap';
 import useAxios from 'axios';
-import { v4 as uuidv4 } from 'uuid';
 import GoalUpdate from "../components/GoalUpdate/GoalUpdate.js"
 
 
@@ -31,7 +30,8 @@ function NewGoal(props) {
     console.table(goalState);
     console.log('Button click ...');
 
-    useAxios.post('/api/goals', { goalState }).then((res) => console.log(res));
+    useAxios.post('/api/goals', { goalState }).then((res) =>
+      console.log(res));
 
 
   };
@@ -65,78 +65,82 @@ function NewGoal(props) {
                     onChange={updateState}
                   />
                 </FormGroup>
-                <Form inline>
-                  <FormGroup>
-                    <h4>
-                      I want my
+                <FormGroup>
+                  <h4>
+                    I want my
                     <Col xs="auto">
-                        <Input type="text" name="unitType" id="unitTypeInput" placeholder="Number of Cigarettes" onChange={updateState} />
-                      </Col>
-                      <Col xs="auto">
-                        <Label for="targetTypeSelect">On average or as a total</Label>
-                        <Input type="select" name="targetType" id="targetTypeSelect" onChange={updateState}>
-                          <option id="average">Average</option>
-                          <option id="total">Total</option>
-                        </Input>
-                      </Col>
+                      <Label for="unitTypeInput">Unit</Label>
+                      <Input type="text" name="unitType" id="unitTypeInput" placeholder="Number of Cigarettes" onChange={updateState} />
+                    </Col>
+                    <Col xs="auto">
+                      <Label for="targetTypeSelect">On average or as a total</Label>
+                      <Input type="select" name="targetType" id="targetTypeSelect" onChange={updateState}>
+                        <option id="average">Average</option>
+                        <option id="total">Total</option>
+                      </Input>
+                    </Col>
                     Per
                     <Col xs="auto">
-                        <Label for="averageTypeSelect">Average Period</Label>
-                        <Input type="select" name="avgPeriod" id="averageTypeSelect" onChange={updateState}>
-                          <option id="day">Day</option>
-                          <option id="week">Week</option>
-                          <option id="week">Month</option>
-                        </Input>
-                      </Col>
+                      <Label for="averageTypeSelect">Average Period</Label>
+                      <Input type="select" name="avgPeriod" id="averageTypeSelect" onChange={updateState}>
+                        <option id="day">Day</option>
+                        <option id="week">Week</option>
+                        <option id="week">Month</option>
+                      </Input>
+                    </Col>
                     To
                     <Col xs="auto">
-                        <Label for="goalTypeSelect">Select</Label>
-                        <Input type="select" name="goalType" id="goalTypeSelect" onChange={updateState}>
-                          <option id="reduce">Reduce</option>
-                          <option id="increase">Increase</option>
-                          <option id="repeat">Repeat</option>
-                        </Input>
-                      </Col>
+                      <Label for="goalTypeSelect">Select</Label>
+                      <Input type="select" name="goalType" id="goalTypeSelect" onChange={updateState}>
+                        <option id="reduce">Reduce</option>
+                        <option id="increase">Increase</option>
+                        <option id="repeat">Repeat</option>
+                      </Input>
+                    </Col>
                     To
                     <Col xs="auto">
-                        <Label for="goalTypeSelection">Select</Label>
-                        <Input type="text" name="target" id="targetInput" placeholder="1" onChange={updateState} />
-                      </Col>
+                      <Label for="goalTypeSelection">Target Units per Period</Label>
+                      <Input type="text" name="target" id="targetInput" placeholder="1" onChange={updateState} />
+                    </Col>
                     By
                     <Col xs="auto">
-                        <Label for="dateInput">Completion Date</Label>
-                        <Input
-                          type="date"
-                          name="completionDate"
-                          placeholder="date placeholder"
-                          onChange={updateState}
-                        />
-                      </Col>
-                    </h4>
-                  </FormGroup>
-                </Form>
-                <FormGroup tag="fieldset">
-                  <legend>Consequence</legend>
-                  <FormGroup check>
-                    <Label check>
-                      <Input type="radio" name="consequence1" id="consequence1" />{' '}
-                            Message someone if I succeed
-                        </Label>
-                    <Input type="text" name="phoneNumber1" id="phoneNumber1" placeholder="123-456-7890" />
-                  </FormGroup>
-                  <FormGroup check>
-                    <Label check>
-                      <Input type="radio" name="consequence2" id="consequence2" />{' '}
-                          Message someone if I fail
-                        </Label>
-                    <Input type="text" name="phoneNumber2" id="phoneNumber2" placeholder="123-456-7890" />
-                  </FormGroup>
-                  <FormGroup check>
-                    <Label check>
-                      <Input type="radio" name="noConsequence" id="noConsequence" />{' '}
-                           No Consequence
-                        </Label>
-                  </FormGroup>
+                      <Label for="dateInput">Completion Date</Label>
+                      <Input
+                        type="date"
+                        name="completionDate"
+                        placeholder="date placeholder"
+                        onChange={updateState}
+                      />
+                    </Col>
+
+
+                    <Label for="consequenceTargetContact">Contact Email</Label>
+                    <Input
+                      type="email"
+                      name="consequenceTargetContact"
+                      placeholder="MyMom@MyMomsEmail.com"
+                      onChange={updateState}
+                    />
+
+                    <Label for="successMessage">Success Message</Label>
+                    <Input
+                      type="text"
+                      name="successMessage"
+                      placeholder="Hey Mom I totally crushed my goal!"
+                      onChange={updateState}
+                    />
+
+                    <Label for="failureMessage">Failure Message</Label>
+                    <Input
+                      type="text"
+                      name="failureMessage"
+                      placeholder="OOPS.  I didn't succeed."
+                      onChange={updateState}
+                    />
+
+
+                  </h4>
+
                 </FormGroup>
                 <Button onClick={handleClick}> Submit</Button>
 
