da95f4cb0c931472992bfbc9a922756ed99f736d
BetterBusinessBrian <bman1402@gmail.com>
2021-02-18 19:47:46 -0500

    fixed goal model

--- client/src/views/GoalInput.js
+++ client/src/views/GoalInput.js
@@ -3,6 +3,7 @@ import { Row, Col, Card, CardHeader, CardTitle, Button, Form, FormGroup, Label, 
 import useAxios from 'axios';
 import GoalUpdate from "../components/GoalUpdate/GoalUpdate.js"
 
+
 function NewGoal(props) {
 
   //TODO:  
@@ -22,7 +23,6 @@ function NewGoal(props) {
     successMessage: "",
     failureMessage: "",
     goalLog: []
-
   });
 
   const handleClick = (e) => {
