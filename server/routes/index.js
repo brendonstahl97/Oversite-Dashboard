9d09e03e42a844c620db6ecbfeee283800f72227
emelanson <elijahmelanson@gmail.com>
2021-02-20 14:56:59 -0500

    Seperated goal routes into goalRoutes.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,11 @@
 const path = require("path");
 const router = require("express").Router();
 const authRoutes = require("./authRoutes");
-const goalController = require("../controllers/goalController");
+const goalRoutes = require("./goalRoutes");
 
 
 router.use("/api/auth", authRoutes);
-router.post("/api/goals", goalController.create);
-router.post("/api/goals/update", goalController.addData);
-router.post("/api/goals/list/:_id", goalController.listGoals);
-
+router.use("/api/goal", goalRoutes)
 
 // If no API routes are hit, send the React app
 router.use((req, res) => {
