cf5b56686f4ca8dc01627f5ce3b041653c77fd38
brendonstahl97 <brendonstahl97@gmail.com>
2021-02-20 10:54:14 -0500

    dynamic goal rendering on the goal list is now functional

---
+++ client/src/components/GoalList/GoalList.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Table } from 'reactstrap';
+import GoalListItem from "../GoalListItem/GoalListItem.js";
+
+export default function GoalList(props) {
+
+    let goalData = [
+        {
+            goalName: "Quit Smoking",
+            targetType: "Total",
+            target: "0",
+            habit: "reduce",
+            completionDate: "01/01/2022"
+        }
+    ];
+
+    return (
+        <Table className="tablesorter" responsive>
+            <thead className="text-primary">
+                <tr>
+                    <th>Name</th>
+                    <th>Target Type</th>
+                    <th>Target</th>
+                    <th>Habit</th>
+                    <th className="text-center">Completion Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {goalData.map((goal, index) => {
+                    return (
+                        <GoalListItem
+                            goal={goal}
+                        />
+                    )
+                })}
+            </tbody>
+        </Table>
+    );
+};
