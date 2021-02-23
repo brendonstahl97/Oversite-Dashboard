afec50aea1f3811a16d1a81f218ff008a84b8fc7
brendonstahl97 <brendonstahl97@gmail.com>
2021-02-23 02:22:20 -0500

    Removed Target Type from view goals page list

--- client/src/components/GoalListItem/GoalListItem.js
+++ client/src/components/GoalListItem/GoalListItem.js
@@ -1,13 +1,12 @@
 import React from 'react'
 
 export default function GoalListItem(props) {
-    const { goalName, unitType, goalType, targetType, target, completionDate } = props.goal;
+    const { goalName, unitType, goalType, target, completionDate } = props.goal;
     return (
         <tr>
             <td>{goalName}</td>
             <td>{unitType}</td>
             <td>{goalType}</td>
-            <td>{targetType}</td>
             <td>{target}</td>
             <td>{completionDate}</td>
         </tr>
